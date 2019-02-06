87f112c35906c78a836ccbed084133100cd547b3
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-05 17:16:38 -0800

    Deleted components not needed for nights.hacksc.com

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/footer.scss'
-
-import React from "react"
-
-export default () => (
-  <footer className="footer has-text-centered">
-    <p>&copy; HackSC 2019</p>
-  </footer>
-)
--- src/components/Question.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default (props) => (
-  <div className="question">
-    <h3>{props.question}</h3>
-    <p>{props.answer}</p>
-  </div>
-)
