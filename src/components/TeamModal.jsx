79fccf69a4df22fb1b8fb6d7d6ade9bc839111d4
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-07 15:42:59 -0800

    Yay we're a team

--- src/components/TeamModal.jsx
+++ src/components/TeamModal.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "../styles/teamModal.scss";
+
 export default (props) => {
   const modalClass = (props.isActive) ? 'modal is-active' : 'modal'
 
@@ -8,20 +10,48 @@ export default (props) => {
       <div className="modal-background" onClick={props.hideModal}></div>
 
       <div className="modal-content">
-        <div className="box">
+        <div className="box team-modal">
           <h1>Our Team</h1>
 
           <h2>E-Board</h2>
+          Stiven.
+          Jack.
+          Caleb.
+          Sam.
+          Wilhelm.
 
           <h2>Logistics</h2>
+          Anjan.
+          Anushka.
+          Lauralee.
+          Jane.
+          Wilhelm.
+          Daniel.
 
           <h2>Hacker Experience</h2>
+          Sam.
+          Michelle.
+          Katie.
+          Andreas.
+          Arielle.
 
           <h2>Sponsorship</h2>
+          Caleb.
+          Ralph.
+          Emily.
+          Sakura.
 
           <h2>Marketing</h2>
+          Jenny.
+          Diya.
+          Benton.
+          Fedja.
 
           <h2>Special Projects</h2>
+          Devika.
+          Maggie.
+          Forrest.
+          Alex.
         </div>
       </div>
 
