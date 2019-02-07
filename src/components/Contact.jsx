1799cbd2b0188848f04c377a023467ee87b577d0
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-07 14:26:33 -0800

    Updated social links w/ new and better icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 
-import facebook from '../assets/facebook.png';
-import instagram from '../assets/instagram.png';
-import email from '../assets/envelope.png';
+import facebook from '../assets/social/facebook.svg';
+import twitter from '../assets/social/twitter.svg';
+import medium from '../assets/social/medium.svg';
+import instagram from '../assets/social/instagram.svg';
+import email from '../assets/social/email.svg';
 
 export default () => (
   <div>
@@ -13,6 +15,8 @@ export default () => (
     <p className="social-links">
       <a href="https://www.facebook.com/hackscofficial/" target="_blank" rel="noopener noreferrer"><img src={facebook} alt="Facebook" /></a>
       <a href="https://instagram.com/hackscofficial" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="Instagram" /></a>
+      <a href="https://twitter.com/hackscofficial" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter" /></a>
+      <a href="https://medium.com/@hacksc" target="_blank" rel="noopener noreferrer"><img src={medium} alt="Medium" /></a>
       <a href="mailto:team@hacksc.com"><img src={email} alt="Email" /></a>
     </p>
   </div>
