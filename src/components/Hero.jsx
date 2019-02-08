ea85d4dbe4d158d9ec48e32c98a4e28f96ef94d7
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-07 18:51:50 -0800

    Updated hero based off Jenny's design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,9 @@ import '../styles/footer.scss'
 
 import React from "react"
 
-import wireFlower from '../assets/wire-flower_white.svg';
+import hacksc from '../assets/hacksc.svg';
 import clickDown from '../assets/angle-down.svg';
+import city from '../assets/city.png';
 
 import { Link } from 'react-scroll';
 
@@ -15,17 +16,18 @@ export default (props) => (
       <div className="hero-body">
         <div className="container">
           <div className="columns is-vcentered">
-            <div className="column is-half has-text-centered">
-              <img src={wireFlower} className="logo" alt="HackSC Logo" />
-            </div>
-
             <div className="column is-half details-column">
-              <h1 className="title">HackSC</h1>
+              <img src={hacksc} alt="HackSC" className="hacksc"/>
+              <hr/>
               <h2 className="title">April 12-14, 2019</h2>
-              <h2 className="title">Los Angeles, CA</h2>
+              <h2 className="title city">Los Angeles, CA</h2>
               <p className="subtitle">Hackathon, but we do it better.<br/>Hosted by USC, baby.</p>
 
-              <button className="button is-primary is-medium apply-button">Applications Opening Feb 10</button>
+              <button className="button is-primary is-medium apply-button">Applications Open Feb 10</button>
+            </div>
+
+            <div className="column is-half graphic-col">
+              <img src={city} alt="City HackSC graphic" className="city-graphic" />
             </div>
           </div>
         </div>
