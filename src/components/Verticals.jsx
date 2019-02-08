b92e37c01dffc2ffa0268b1c7aaa0213314d1d8a
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-07 21:55:15 -0800

    Another wording change weeooweeooo

--- src/components/Verticals.jsx
+++ src/components/Verticals.jsx
@@ -6,7 +6,7 @@ export default (props) => (
       <div className="column">
         <h3>Entrepreneurship</h3>
 
-        <p>Everything's gotta start somewhere. With a team you come in with, or one you make while at HackSC, establish working relationships which will extend long past the weekend of April 12. Under 'Entrepreneurship,' we want to establish a space where you can create products, not projects.</p>
+        <p>Everything's gotta start somewhere. With a team you come in with, or one you make while at HackSC, establish working relationships which will extend long past the weekend of April 12. Under 'Entrepreneurship,' we want to establish a space where you can create products, not just projects.</p>
       </div>
 
       <div className="column">
