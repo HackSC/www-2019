c9824fd3fcdbbdda12f49725a5f75ff299f74cf5
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-03-18 21:28:00 -0700

    Apps are now closed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ export default (props) => (
               <h2 className="title city">Los Angeles, CA</h2>
               <p className="subtitle">Hackathon, but we do it better.<br/>Hosted by USC, baby.</p>
 
-              <a className="button is-primary is-medium apply-button" href="https://apply.hacksc.com">Apply Now!</a>
+              <button className="button is-disabled is-medium apply-button">Apps Closed</button>
 
               <div className="vol-men-forms">
                 <a className="button form-button" href="https://hacksc.typeform.com/to/eGxqro" target="_blank" rel="noopener noreferrer">Volunteer!</a>
@@ -31,7 +31,7 @@ export default (props) => (
               </div>
 
               <p className="due-dates">
-                <b>All Apps Due: </b> March 10, 2019
+                <b>Applications for HackSC 2019 are now closed.</b> However, it's still not too late to apply as a mentor or volunteer! Click one of the above buttons to apply for either
               </p>
             </div>
 
