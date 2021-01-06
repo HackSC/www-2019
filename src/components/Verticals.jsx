984c0ae26f0ce14977adbe052206597181d8d554
Andreas Bigger <abigger87@gmail.com>
2021-01-06 13:27:47 -0800

    bumped dependencies

--- src/components/Verticals.jsx
+++ src/components/Verticals.jsx
@@ -1,25 +1,36 @@
-import React from "react"
+import React from 'react'
 
-export default (props) => (
+export default () => (
   <div>
-    <div className="columns">
-      <div className="column">
+    <div className='columns'>
+      <div className='column'>
         <h3>Entrepreneurship</h3>
 
-        <p>Everything's gotta start somewhere. With a team you come in with, or one you make while at HackSC, establish working relationships which will extend long past the weekend of April 12. Under 'Entrepreneurship,' we want to establish a space where you can create products, not just projects.</p>
+        <p>Everything's gotta start somewhere. 
+          With a team you come in with, or one you make while at HackSC, 
+          establish working relationships which will extend long past 
+          the weekend of April 12. Under "Entrepreneurship,"" we want to 
+          establish a space where you can create products, not just projects.</p>
       </div>
 
-      <div className="column">
+      <div className='column'>
         <h3>Entertainment</h3>
 
-        <p>You're in LA, what did you expect? We're not looking for Ryan Gosling's replacement, his spot is full. However, there is plenty of room in regards to other tech developments across all fields: music,  gaming and interactive media, and naturally film and VFX.</p>
+        <p>You're in LA, what did you expect? We're not looking for Ryan 
+          Gosling's replacement, his spot is full. However, there is plenty 
+          of room in regards to other tech developments across all fields: 
+          music,  gaming and interactive media, and naturally film and VFX.</p>
       </div>
 
-      <div className="column">
+      <div className='column'>
         <h3>Transportation</h3>
 
-        <p>It's about time someone invented teleportation... or effective LA public transit. You decide. Mobility comes in many ways besides current public transit and ridesharing programs, and so it's up to you to take the initiative to develop new advances in transportation in an increasingly globalized world.</p>
+        <p>It's about time someone invented teleportation... or effective 
+          LA public transit. You decide. Mobility comes in many ways 
+          besides current public transit and ridesharing programs, 
+          and so it's up to you to take the initiative to develop new 
+          advances in transportation in an increasingly globalized world.</p>
       </div>
     </div>
   </div>
-)
+);
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './styles/index.scss';
-import App from './App';
 import ReactGA from 'react-ga';
-import { hydrate, render } from "react-dom";
+import { hydrate, render } from 'react-dom';
+import App from './App';
 
 ReactGA.initialize('UA-127488741-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
 } else {
